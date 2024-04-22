541ea5af2060730eb11424081a2500f5ffc9c29b
Tyson <TysonS.417@gmail.com>
2024-04-21 19:18:05 -0700

    remove unused ollama schemas

--- src/lib/utils/ollama.ts
+++ src/lib/utils/ollama.ts
@@ -3,23 +3,6 @@ import { z } from 'zod';
 const modelEnumSchema = z.enum(['llama3']).default('llama3');
 const roleEnumSchema = z.enum(['user', 'assistant']);
 
-// const ollamaMessageContentSchema = z.object({
-//   data: z
-//     .object({
-//       property: z.enum(['answer']),
-//       value: z.string(),
-//       related_info: z.string().nullable()
-//     })
-//     .array(),
-//   errors: z
-//     .object({
-//       code: z.number(),
-//       message: z.string()
-//     })
-//     .array()
-// });
-
-// type OllamaMessageContent = z.infer<typeof ollamaMessageContentSchema>;
 
 export const ollamaResponseSchema = z.object({
 	model: modelEnumSchema,
@@ -29,13 +12,12 @@ export const ollamaResponseSchema = z.object({
 		role: roleEnumSchema.exclude(['user']),
 		content: z.string()
 	})
-}).passthrough();
+});
 
 export type OllamaResponse = z.infer<typeof ollamaResponseSchema>;
 
 export const ollamaRequestSchema = z.object({
 	model: modelEnumSchema,
-	format: z.enum(['json']).default('json'),
 	stream: z.boolean().default(true),
 	messages: z
 		.object({
