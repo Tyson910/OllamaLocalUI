2acb57da520c0dcb4f1c48ee6feb412c4ccead70
Tyson <TysonS.417@gmail.com>
2024-04-22 20:38:58 -0700

    create newUserSchema

--- src/lib/utils/sqlite-tables-validators.ts
+++ src/lib/utils/sqlite-tables-validators.ts
@@ -3,11 +3,16 @@ import { z } from 'zod';
 // User schema
 export const UserSchema = z.object({
 	id: z.number().optional(),
-	displayName: z.string().min(1).max(255),
+	displayName: z
+		.string()
+		.min(1, { message: 'Please provide a value' })
+		.max(255, { message: 'This username is too long. Try Again' }),
 	createdAt: z.string().datetime({ offset: true }),
 	updatedAt: z.string().datetime({ offset: true }).optional(),
 	deletedAt: z.string().datetime({ offset: true }).optional()
 });
+
+export const newUserSchema = UserSchema.pick({ displayName: true });
 
 // Convo schema
 export const ConvoSchema = z.object({
