5cf7972227a200ec81112e3051eb5fef7dd7740b
Tyson <TysonS.417@gmail.com>
2024-04-22 20:56:33 -0700

    set up 'new-user' action

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,35 @@
+import type { PageServerLoad, Actions } from './$types';
+import { message, superValidate } from 'sveltekit-superforms';
+import { zod as SuperFormZod } from 'sveltekit-superforms/adapters';
+
+import { newUserSchema } from '$lib/utils/sqlite-tables-validators';
+import { db } from '$lib/utils/kysely';
+
+export const load = (async () => {
+	return {};
+}) satisfies PageServerLoad;
+
+export const actions = {
+	'new-user': async (event) => {
+		// TODO log the user in
+		const form = await superValidate(event.request, SuperFormZod(newUserSchema));
+
+		if (!form.valid) {
+			// Will return fail(400, { form }) since form isn't valid
+			return message(form, { text: 'Invalid form', type: 'error' });
+		}
+
+		try {
+			await db
+				.insertInto('user')
+				.values({ ...form.data, created_at: new Date().toISOString() })
+				.executeTakeFirst();
+
+			return message(form, { text: 'Success!', type: 'success' });
+		} catch (error) {
+			console.error(error);
+			return message(form, { text: 'Unexpected Error', type: 'error' });
+
+		}
+	}
+} satisfies Actions;
