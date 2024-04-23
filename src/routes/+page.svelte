f0b90c82e1fa9ecec04de5e7b791ed77db64b6c4
Tyson <TysonS.417@gmail.com>
2024-04-22 20:06:32 -0700

    outline homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,52 @@
 <script lang="ts">
-	import { ollamaResponseSchema, ollamaRequestSchema } from '$lib/utils/ollama';
-	import { marked } from 'marked';
-
-
-	let searchInput =
-		"I'm testing a REST API to ask you questions. Respond with some facts about the Sonoran Desert.";
-	let agentResponse = '';
-
-	async function readData() {
-		const validatedRequestBody = ollamaRequestSchema.parse({
-			messages: [
-				{
-					role: 'user',
-					content: searchInput
-				}
-			]
-		});
-
-		console.log('submitting request');
-		const response = await fetch('http://localhost:11434/api/chat', {
-			method: 'POST',
-			body: JSON.stringify(validatedRequestBody)
-		});
-		if (!response.body) throw new Error('Missing request body!');
-		try {
-			console.log('parsing body');
-			const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-			while (true) {
-				const { value } = await reader.read();
-				if (!value) {
-					throw new Error();
-				}
-				// TODO: clean up handling the 'done' status...not getting the full done response...
-				const validatedResponse = ollamaResponseSchema.parse(JSON.parse(value));
-				agentResponse += validatedResponse.message.content;
-				if (validatedResponse.done) break;
-			}
-		} catch (error) {
-			console.error(error);
-		}
-
-		console.log('done!');
-	}
+	import AtSymbolIcon from 'virtual:icons/heroicons/at-symbol';
+	// let files: FileList | null = null;
+	// $: if (files) {
+	// 	// Note that `files` is of type `FileList`, not an Array:
+	// 	// https://developer.mozilla.org/en-US/docs/Web/API/FileList
+	// 	console.log(files);
+	// 	for (const file of files) {
+	// 		console.log(`${file.name}: ${file.size} bytes`);
+	// 	}
+	// }
+	// $: avatarImgPreview = files?.item(0);
 </script>
 
-<form on:submit|preventDefault={readData} class="max-w-screen-lg mx-auto my-40">
+<form class="max-w-screen-xl mx-auto flex flex-col py-10">
 	<div>
-		<label for="chat-message" class="block text-sm font-medium leading-6 text-gray-900"
-			>Search</label
-		>
-		<div class="mt-2">
+		<label for="displayName" class="text-sm font-medium leading-6 text-gray-900">
+			Display Name
+		</label>
+		<div class="relative mt-2 rounded-md shadow-sm">
+			<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+				<AtSymbolIcon class="size-5 text-gray-400" aria-hidden="true" />
+			</div>
 			<input
 				type="text"
-				name="chat-message"
-				id="chat-message"
-				class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-				bind:value={searchInput}
+				name="displayName"
+				id="displayName"
+				class="w-full rounded-md border-0 py-1.5 pl-9 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
 			/>
 		</div>
 	</div>
-	<button type="submit">Submit</button>
-</form>
 
-<div class="max-w-screen-lg mx-auto">
-	{@html marked.parse(agentResponse)}
-</div>
+	<!-- add avatar image later! -->
+	<!-- <div>
+		<label for="avatar">Avatar Image</label>
+		<input accept="image/*" type="file" id="avatar" bind:files />
+		{#if avatarImgPreview != null}
+			<img
+				id="blah"
+				src={URL.createObjectURL(avatarImgPreview)}
+				alt="Your avatar"
+				class="inline-block size-14 rounded-full object-cover"
+			/>
+		{/if}
+	</div> -->
+	<button
+		type="submit"
+		class="rounded-md bg-indigo-600 mt-5 self-end px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+	>
+		Submit
+	</button>
+</form>
