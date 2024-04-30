018bdfc0034210afd46a5dd8e49ad39b901011f6
Tyson <TysonS.417@gmail.com>
2024-04-29 21:19:07 -0700

    clean up chat page.svelte

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,58 +1,18 @@
 <script lang="ts">
-	import { ollamaResponseSchema, ollamaRequestSchema } from '$lib/utils/ollama';
-	import { marked } from 'marked';
-
 	let searchInput =
 		"I'm testing a REST API to ask you questions. Respond with some facts about the Sonoran Desert.";
-	let agentResponse = '';
-
-	async function readData() {
-		const validatedRequestBody = ollamaRequestSchema.parse({
-			messages: [
-				{
-					role: 'user',
-					content: searchInput
-				}
-			]
-		});
-
-		console.log('submitting request');
-		const response = await fetch('http://localhost:11434/api/chat', {
-			method: 'POST',
-			body: JSON.stringify(validatedRequestBody)
-		});
-		if (!response.body) throw new Error('Missing request body!');
-		try {
-			console.log('parsing body');
-			const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-			while (true) {
-				const { value } = await reader.read();
-				if (!value) {
-					throw new Error();
-				}
-				// TODO: clean up handling the 'done' status...not getting the full done response...
-				const validatedResponse = ollamaResponseSchema.parse(JSON.parse(value));
-				agentResponse += validatedResponse.message.content;
-				if (validatedResponse.done) break;
-			}
-		} catch (error) {
-			console.error(error);
-		}
-
-		console.log('done!');
-	}
 </script>
 
-<form on:submit|preventDefault={readData} class="max-w-screen-lg mx-auto my-40">
+<form class="max-w-screen-lg mx-auto my-40">
 	<div>
-		<label for="chat-message" class="block text-sm font-medium leading-6 text-gray-900"
-			>Search</label
+		<label for="convo-name" class="block text-sm font-medium leading-6 text-gray-900"
+			>New Convo</label
 		>
 		<div class="mt-2">
 			<input
 				type="text"
-				name="chat-message"
-				id="chat-message"
+				name="convo-name"
+				id="convo-name"
 				class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 				bind:value={searchInput}
 			/>
@@ -61,6 +21,3 @@
 	<button type="submit">Submit</button>
 </form>
 
-<div class="max-w-screen-lg mx-auto">
-	{@html marked.parse(agentResponse)}
-</div>
