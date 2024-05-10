e350ceac714acdbf94e1cd86f9a74298b1009122
Tyson <TysonS.417@gmail.com>
2024-05-09 21:40:01 -0700

    bring in styles & utils

---
+++ astro-app/src/utils/ollama.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+const modelEnumSchema = z.enum(['llama3']).default('llama3');
+const roleEnumSchema = z.enum(['user', 'assistant']);
+
+
+export const ollamaResponseSchema = z.object({
+	model: modelEnumSchema,
+	created_at: z.string().datetime({ offset: true }),
+	done: z.boolean(),
+	message: z.object({
+		role: roleEnumSchema.exclude(['user']),
+		content: z.string()
+	})
+});
+
+export type OllamaResponse = z.infer<typeof ollamaResponseSchema>;
+
+export const ollamaRequestSchema = z.object({
+	model: modelEnumSchema,
+	stream: z.boolean().default(true),
+	messages: z
+		.object({
+			role: roleEnumSchema,
+			content: z.string()
+		})
+		.array()
+});
+
+// type OllamaRequest = z.infer<typeof ollamaRequestSchema>;
+
