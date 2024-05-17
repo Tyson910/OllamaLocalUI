867e844039105388282d9b7a59762668781d38f2
Tyson <TysonS.417@gmail.com>
2024-05-16 20:23:28 -0700

    set up newMessage schema

--- astro-app/src/actions/index.ts
+++ astro-app/src/actions/index.ts
@@ -1,6 +1,7 @@
 import { defineAction, ActionError, getApiContext } from 'astro:actions';
 import { newUserSchema, UserSchema } from '@utils/sqlite-tables-validators';
 import { db } from '@utils/kysely';
+import { newMessageSchema } from '@utils/ollama';
 
 export const server = {
 	newUser: defineAction({
@@ -50,6 +51,47 @@ export const server = {
 				throw new ActionError({
 					code: 'INTERNAL_SERVER_ERROR',
 					message: `Unable to sign in.`
+				});
+			}
+		}
+	}),
+	newMessage: defineAction({
+		input: newMessageSchema,
+		handler: async (input) => {
+			try {
+				const apiContext = getApiContext();
+
+				if (!apiContext?.params?.convo_id) {
+					throw new ActionError({
+						code: 'BAD_REQUEST',
+						message: `Unable to create new message`
+					});
+				}
+				const result = await db
+					.insertInto('message')
+					.values({
+						role: 'user',
+						content: input.content,
+						convo_id: apiContext.params.convo_id,
+						created_at: new Date().toISOString()
+					})
+					.returning(['message.content', 'message.updated_at', 'message.role'])
+					.executeTakeFirst();
+				if (!result) {
+					throw new ActionError({
+						code: 'INTERNAL_SERVER_ERROR',
+						message: `An unexpected error has occured.`
+					});
+				}
+				return result;
+			} catch (error) {
+				console.error(error);
+				if (error instanceof ActionError) {
+					throw error;
+				}
+				throw new ActionError({
+					code: 'INTERNAL_SERVER_ERROR',
+					message: `An unexpected error has occured.`
 				});
 			}
 		}
