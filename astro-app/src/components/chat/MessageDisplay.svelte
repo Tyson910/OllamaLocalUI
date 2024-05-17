cd738778f3c7c4bcc69885b131a7433d556c688c
Tyson <TysonS.417@gmail.com>
2024-05-16 20:29:49 -0700

    create MessageDisplay component

---
+++ astro-app/src/components/chat/MessageDisplay.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	import type { DB } from '@utils/kysely';
+
+	export let messages: DB['message'][];
+</script>
+
+<div>
+	{#each messages as message}
+		<p>Role: {message.role}</p>
+		{@html marked.parse(message.content)}
+	{/each}
+</div>
