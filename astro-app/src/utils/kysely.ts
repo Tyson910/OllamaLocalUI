5e9ff9e5f52bf478cb189c75a047bf2a994ff747
Tyson <TysonS.417@gmail.com>
2024-05-16 20:30:01 -0700

    export DB types

--- astro-app/src/utils/kysely.ts
+++ astro-app/src/utils/kysely.ts
@@ -1,15 +1,17 @@
-import type { DB  } from 'kysely-codegen';
+import type { DB } from 'kysely-codegen';
 import SQLite from 'better-sqlite3';
-import { Kysely, SqliteDialect } from 'kysely'
+import { Kysely, SqliteDialect } from 'kysely';
 
 const dialect = new SqliteDialect({
-  database: new SQLite(import.meta.env.DATABASE_URL),
-})
+	database: new SQLite(import.meta.env.DATABASE_URL)
+});
 
-// Database interface is passed to Kysely's constructor, and from now on, Kysely 
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
 // knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
 // to communicate with your database.
 export const db = new Kysely<DB>({
-  dialect,
-})
+	dialect
+});
+
+export type { DB };
