6bd11b01e2a95ee0f539034ed598d643ecb9440d
Tyson <TysonS.417@gmail.com>
2024-05-22 20:24:23 -0700

    use named export GuestLayout

--- laravel-app/resources/js/Layouts/GuestLayout.tsx
+++ laravel-app/resources/js/Layouts/GuestLayout.tsx
@@ -1,8 +1,8 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
+import type { PropsWithChildren } from 'react';
 
-export default function Guest({ children }: PropsWithChildren) {
+export function GuestLayout({ children }: PropsWithChildren) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
