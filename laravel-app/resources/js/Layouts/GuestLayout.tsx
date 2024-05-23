f3d8518835101f30639625c0bbb5e780c98a2fa0
Tyson <TysonS.417@gmail.com>
2024-05-22 20:29:52 -0700

    use named export ApplicationLogo

--- laravel-app/resources/js/Layouts/GuestLayout.tsx
+++ laravel-app/resources/js/Layouts/GuestLayout.tsx
@@ -1,4 +1,4 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import { ApplicationLogo } from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 import type { PropsWithChildren } from 'react';
 
