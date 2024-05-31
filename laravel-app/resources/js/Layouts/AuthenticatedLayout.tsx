7a862401e831dbc23d306397b8482960e1fd93ca
Tyson <TysonS.417@gmail.com>
2024-05-30 22:34:45 -0700

    use named export NavLink

--- laravel-app/resources/js/Layouts/AuthenticatedLayout.tsx
+++ laravel-app/resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,7 +1,7 @@
 import { useState, PropsWithChildren, ReactNode } from 'react';
 import { ApplicationLogo } from '@/Components/ApplicationLogo';
 import { Dropdown } from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
+import { NavLink } from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 import type { User } from '@/types';
