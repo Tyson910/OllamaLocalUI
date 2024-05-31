153179429ed63679beaf30139d83c487b7de819d
Tyson <TysonS.417@gmail.com>
2024-05-30 22:30:49 -0700

    use named export InputLabel

--- laravel-app/resources/js/Pages/Auth/Register.tsx
+++ laravel-app/resources/js/Pages/Auth/Register.tsx
@@ -1,7 +1,7 @@
 import { useEffect, FormEventHandler } from 'react';
 import { GuestLayout } from '@/Layouts/GuestLayout';
 import { InputError } from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
+import { InputLabel } from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import { Head, Link, useForm } from '@inertiajs/react';
