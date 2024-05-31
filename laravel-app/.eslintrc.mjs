ec2ef52948615b5d98098f45936eda20a392fb81
Tyson <TysonS.417@gmail.com>
2024-05-30 21:58:39 -0700

    set up eslint & prettier

---
+++ laravel-app/.eslintrc.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals';
+// import pluginJs from '@eslint/js';
+// import tseslint from 'typescript-eslint';
+// import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+
+/** @type { import("eslint").Linter.Config } */
+export default {
+  root: true,
+  globals,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
