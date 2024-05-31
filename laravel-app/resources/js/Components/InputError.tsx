d29e98c41dd4950c65bdc15557cfcf68528bcb16
Tyson <TysonS.417@gmail.com>
2024-05-30 21:59:46 -0700

    prettier format

--- laravel-app/resources/js/Components/InputError.tsx
+++ laravel-app/resources/js/Components/InputError.tsx
@@ -1,9 +1,13 @@
 import type { HTMLAttributes } from 'react';
 
-export function InputError({ message, className = '', ...props }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
-    return message ? (
-        <p {...props} className={'text-sm text-red-600 dark:text-red-400 ' + className}>
-            {message}
-        </p>
-    ) : null;
+export function InputError({
+  message,
+  className = '',
+  ...props
+}: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
+  return message ? (
+    <p {...props} className={'text-sm text-red-600 dark:text-red-400 ' + className}>
+      {message}
+    </p>
+  ) : null;
 }
