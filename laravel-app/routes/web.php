000e9e73bdf0a3a5b81f0f9cc562be2bd1316c7a
Tyson <TysonS.417@gmail.com>
2024-05-30 21:27:37 -0700

    pint fixes

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ConvoController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
