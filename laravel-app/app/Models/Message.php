7f2abd3d23d41953a22abd6160f89ec4486c1756
Tyson <TysonS.417@gmail.com>
2024-05-31 18:03:58 -0700

    add belongsTo relations to Convo and Message

--- laravel-app/app/Models/Message.php
+++ laravel-app/app/Models/Message.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Message extends Model
 {
     use HasFactory, HasUlids;
+
+    public function convo(): BelongsTo
+    {
+        return $this->belongsTo(Convo::class);
+    }
 
     /**
      * The attributes that are mass assignable.
