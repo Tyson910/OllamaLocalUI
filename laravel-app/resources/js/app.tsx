f9d66c8b4fd67f4e5d1d0d6eb71a0284a438b907
Tyson <TysonS.417@gmail.com>
2024-06-03 21:04:06 -0700

    place react query provider at the root of the app

--- laravel-app/resources/js/app.tsx
+++ laravel-app/resources/js/app.tsx
@@ -4,8 +4,11 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// Create a client
+const queryClient = new QueryClient();
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
@@ -14,7 +17,11 @@ createInertiaApp({
   setup({ el, App, props }) {
     const root = createRoot(el);
 
-    root.render(<App {...props} />);
+    root.render(
+      <QueryClientProvider client={queryClient}>
+        <App {...props} />
+      </QueryClientProvider>,
+    );
   },
   progress: {
     color: '#4B5563',
