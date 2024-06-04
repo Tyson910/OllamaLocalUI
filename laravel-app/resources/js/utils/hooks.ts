403c8d2ac62d7084a244374eb749cfe2bd8c04fa
Tyson <TysonS.417@gmail.com>
2024-06-03 19:51:08 -0700

    first pass at useStreamResponse

--- laravel-app/resources/js/utils/hooks.ts
+++ laravel-app/resources/js/utils/hooks.ts
@@ -1,6 +1,63 @@
 import type { PageProps } from '@/types';
 import { usePage } from '@inertiajs/react';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export function useTypeSafePage() {
   return usePage<PageProps>();
+}
+
+
+export function useStreamResponse({
+  streamCallback,
+}: {
+  streamCallback: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const [responses, setResponses] = useState('');
+  const [data, setData] = useState<string | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const { mutate: startStream } = useMutation({
+    mutationFn: async (messageContent: string) => {
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: messageContent }),
+      });
+
+      if (!response.body) {
+        throw new Error('ReadableStream not supported in this browser.');
+      }
+
+      const reader = response.body.getReader();
+      return reader;
+    },
+    onSuccess: (reader) => {
+      setIsLoading(true);
+      readStream(reader);
+    },
+  });
+
+  async function readStream(reader: ReadableStreamDefaultReader) {
+    async function read() {
+      const { done, value } = await reader.read();
+      if (done) {
+        setIsLoading(false);
+        return;
+      }
+
+      const text = new TextDecoder().decode(value);
+      if (text.includes('END STREAM')) {
+        setData(JSON.parse(text.replace(/.*END STREAM/, '')));
+      } else {
+        setResponses((prev) => prev + text);
+        streamCallback((prevValue) => prevValue + text);
+      }
+      read();
+    }
+    read();
+  }
+
+  return { responses, data, startStream, isLoading };
 }
