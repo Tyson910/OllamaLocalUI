95debeca06845149f961fbd609ce902ecdb844d0
Tyson <TysonS.417@gmail.com>
2024-06-03 21:11:27 -0700

    wire up ollama req/res

--- laravel-app/resources/js/utils/hooks.ts
+++ laravel-app/resources/js/utils/hooks.ts
@@ -7,23 +7,39 @@ export function useTypeSafePage() {
   return usePage<PageProps>();
 }
 
-
 export function useStreamResponse({
   streamCallback,
 }: {
   streamCallback: React.Dispatch<React.SetStateAction<string>>;
 }) {
+  // entire response text
   const [responses, setResponses] = useState('');
+  // data I want to send to consumer/caller
   const [data, setData] = useState<string | undefined>();
   const [isLoading, setIsLoading] = useState(false);
   const { mutate: startStream } = useMutation({
     mutationFn: async (messageContent: string) => {
-      const response = await fetch('/api/generate', {
+      // const ollamaRequestSchema = z.object({
+      //   model: modelEnumSchema,
+      //   stream: z.boolean().default(true),
+      //   messages: z
+      //     .object({
+      //       role: roleEnumSchema,
+      //       content: z.string(),
+      //     })
+      //     .array(),
+      // });
+
+      const response = await fetch('http://127.0.0.1:11434/api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: messageContent }),
+        body: JSON.stringify({
+          messages: [{ content: messageContent, role: 'user' }],
+          stream: true,
+          model: 'llama3',
+        }),
       });
 
       if (!response.body) {
@@ -48,11 +64,14 @@ export function useStreamResponse({
       }
 
       const text = new TextDecoder().decode(value);
-      if (text.includes('END STREAM')) {
-        setData(JSON.parse(text.replace(/.*END STREAM/, '')));
-      } else {
-        setResponses((prev) => prev + text);
-        streamCallback((prevValue) => prevValue + text);
+
+      try {
+        // TODO: use zod schema to parse these responses
+        const obj = JSON.parse(text);
+        setResponses((prev) => prev + obj?.message?.content);
+        streamCallback((prevValue) => prevValue + obj?.message?.content);
+      } catch (err) {
+        console.log(err);
       }
       read();
     }
