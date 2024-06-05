0a407b8eb0c1d60decff4e727d9f9c826f6fdf2a
Tyson <TysonS.417@gmail.com>
2024-06-04 23:15:26 -0700

    wire up first pass at chat workflow

--- laravel-app/resources/js/Pages/Convos/[id].tsx
+++ laravel-app/resources/js/Pages/Convos/[id].tsx
@@ -1,33 +1,49 @@
 import type { PageProps } from '@/types';
 import type { Convo, Message } from './types';
-import { AuthenticatedLayout } from '@/Layouts/AuthenticatedLayout';
 
-import { useForm, Head } from '@inertiajs/react';
+import { useForm, Head, router } from '@inertiajs/react';
+import ReactMarkdown from 'react-markdown';
+import { AuthenticatedLayout } from '@/Layouts/AuthenticatedLayout';
 import { InputError } from '@/Components/InputError';
 import { PrimaryButton } from '@/Components/PrimaryButton';
-
-
+import { useStreamResponse } from '@/utils/hooks';
 
 interface ConvoDetailProps extends PageProps {
-  convo: Convo 
+  convo: Convo;
   messages: Message[];
 }
 
-export default function ConvoDetails({ auth, ziggy, convo, messages,  ...rest }: ConvoDetailProps) {
+export default function ConvoDetails({ auth, ziggy, convo, messages, ...rest }: ConvoDetailProps) {
+  const { startStream, isPending, streamResponse, status } = useStreamResponse();
   const { data, setData, post, processing, reset, errors } = useForm({
-    content:
-      "I'm testing a REST API to ask you questions. Respond with some facts about the Sonoran Desert.",
+    content: '',
     convo_id: convo.id,
     role: 'user',
   });
 
-  function submit(e: React.FormEvent<HTMLFormElement>) {
+  function saveStreamedMessageToDB(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    post(route('messages.store'), { onSuccess: () => reset() });
+    // first save new message to DB
+    post(route('messages.store'), {
+      preserveScroll: true,
+      onSuccess: ({ props }) => {
+        // reset the form
+        reset();
+        const updatedMessages = props?.messages as unknown as Message[];
+        // now get Ollama response
+        startStream({
+          convo_id: convo.id,
+          messageContent: [
+            ...updatedMessages.map((message) => ({
+              content: message.content,
+              role: message.role,
+            })),
+          ],
+        });
+      },
+    });
   }
 
-  console.log(convo);
-  console.log(messages)
   return (
     <AuthenticatedLayout user={auth.user}>
       <Head title={convo.title} />
@@ -36,13 +52,14 @@ export default function ConvoDetails({ auth, ziggy, convo, messages,  ...rest }:
         <div className='m'>
           {messages.map((message) => (
             <ReactMarkdown
-              className='odd:bg-green-300 even:bg-green-200 rounded p-5 divide-y-yellow-500 divide-y'
+              className='odd:bg-green-300 even:bg-green-200 rounded p-5'
               key={message.id}
               children={message.content}
             />
           ))}
         </div>
-        <ReactMarkdown className='dark:text-white' children={responses} />
+        <ReactMarkdown className='dark:text-white' children={streamResponse?.message.content} />
+        <form onSubmit={saveStreamedMessageToDB}>
           <textarea
             className='block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'
             onChange={(e) => setData('content', e.target.value)}
@@ -51,12 +68,13 @@ export default function ConvoDetails({ auth, ziggy, convo, messages,  ...rest }:
           <input type='hidden' name='convo_id' value={convo.id} />
           <input type='hidden' name='role' value={'user'} />
           <InputError message={errors.content} className='mt-2' />
+          <p className='dark:text-white'>{status}</p>
           {messages?.length == 0 ? (
-            <PrimaryButton className='mt-4' disabled={processing}>
+            <PrimaryButton className='mt-4' disabled={processing || isPending}>
               New Convo
             </PrimaryButton>
           ) : (
-            <PrimaryButton className='mt-4' disabled={processing}>
+            <PrimaryButton className='mt-4' disabled={processing || isPending}>
               Submit
             </PrimaryButton>
           )}
