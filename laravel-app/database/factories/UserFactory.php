35fa2bbe1a9eaa8ff12a1edddf052557609db5a1
Tyson <TysonS.417@gmail.com>
2024-08-08 10:30:08 -0700

    add uploaded file to user factory

--- laravel-app/database/factories/UserFactory.php
+++ laravel-app/database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -30,6 +31,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'avatar' => UploadedFile::fake()->image('photo1.jpg'),
         ];
     }
 
